364216059e36551592152aa4bfcf8a5c7aa9fc04
Martin Feckie <mfeckie@gmail.com>
2018-05-21 22:29:28 +0800

    Add PUT to API functions

--- lib/http/api.ex
+++ lib/http/api.ex
@@ -1,7 +1,8 @@
 defmodule PinPayments.HTTP.API do
   alias PinPayments.HTTP.ClientBase, as: Client
 
+  def delete(url), do: Client.delete(url)
   def get(url), do: Client.get(url)
   def post(url, params), do: Client.post(url, params)
-
+  def put(url, params), do: Client.put(url, params)
 end

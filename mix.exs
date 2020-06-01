dbd2e73e0c5634745bbdcfe9cc381f1d613590ba
Martin Feckie <mfeckie@gmail.com>
2020-06-01 18:44:04 +0800

    Tweak gun and tesla versions

--- mix.exs
+++ mix.exs
@@ -12,6 +12,7 @@ defmodule PINXS.MixProject do
       """,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       docs: [
         logo: "./images/pin_payments.png",
@@ -34,7 +35,7 @@ defmodule PINXS.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :gun]
     ]
   end
 
@@ -45,10 +46,15 @@ defmodule PINXS.MixProject do
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:exvcr, "~> 0.10", only: :test},
       {:gun, "> 1.3.0"},
-      {:nug, "> 0.3.0", only: [:dev, :test]},
+      {:jason, "> 1.2.0"},
+      {:nug, ">= 0.3.3", only: [:dev, :test]},
       {:httpoison, "~> 1.2.0"},
       {:poison, "~> 3.1"},
-      {:tesla, "~> 1.3"}
+      {:tesla, "> 1.3.0"}
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support", "test/support/test_helpers"]
+  defp elixirc_paths(_), do: ["lib"]
 end

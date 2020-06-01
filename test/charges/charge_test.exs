d4b240741b7a2258f2dc2bd37b3def1fb01f6628
Martin Feckie <mfeckie@gmail.com>
2020-06-01 18:45:44 +0800

    Add tests for usage of Tesla based client

--- test/charges/charge_test.exs
+++ test/charges/charge_test.exs
@@ -4,6 +4,8 @@ defmodule PINXS.Charges.ChargeTest do
   alias PINXS.Charges.Charge
   alias PINXS.Cards.Card
   alias PINXS.Customers.Customer
+  import PINXS.TestHelpers
+  use Nug
 
   setup do
     card = %Card{
@@ -219,6 +221,117 @@ defmodule PINXS.Charges.ChargeTest do
 
         assert declined.error_description ==
                  "The transaction was flagged as possibly fraudulent and subsequently declined"
+      end
+    end
+  end
+
+  describe "new client" do
+    test "Create a charge with full card details", %{card: card, charge: charge} do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/create_charge.fixture") do
+        client = client(address)
+        charge = %{charge | card: card}
+        {:ok, charge} = Charge.create(charge, client)
+        assert charge.amount == 50000
+        assert charge.email == "hagrid@hogwarts.wiz"
+        assert charge.card.token == "card_u2vD9fa5icjORwtWm8t6dw"
+        assert charge.captured == true
+      end
+    end
+
+    # TODO fix -> need a used card token to try and reuse
+    # test "Create a charge with a used token", %{charge: charge, card_token: card_token} do
+    #   with_proxy(PINXS.Client.test_url(), "test/fixtures/charge_with_used_card_token.fixture") do
+    #     client = client(address)
+    #     charge_map = %{charge | card_token: card_token}
+    #     {:error, err} = Charge.create(charge_map, client)
+
+    #     assert err.error_description ==
+    #              "Token already used. Card tokens can only be used once, to create a charge or assign a card to a customer"
+    #   end
+    # end
+
+    test "Create a charge with a card token", %{card: card_map, charge: charge} do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/charge_with_card_token.fixture") do
+        {:ok, card} = Card.create(card_map, client(address))
+
+        charge_map = %{charge | card_token: card.token}
+
+        {:ok, charge} = Charge.create(charge_map, client(address))
+
+        assert charge.captured == true
+        assert charge.amount == 50000
+      end
+    end
+
+    test "Create a charge with a customer token", %{charge: charge, customer: customer} do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/charge_with_customer_token.fixture") do
+        {:ok, customer} = Customer.create(customer, client(address))
+
+        charge_map = %{charge | customer_token: customer.token}
+
+        {:ok, charge} = Charge.create(charge_map, client(address))
+
+        assert charge.captured == true
+        assert charge.amount == 50000
+      end
+    end
+
+    test "Capture a charge", %{charge: charge, card: card} do
+      with_proxy(PINXS.Client.test_url(),"test/fixtures/capture_full_charge.fixture") do
+        {:ok, uncaptured_charge} =
+          Charge.create(%{charge | capture: false, card: card}, client(address))
+
+        assert uncaptured_charge.captured == false
+
+        {:ok, captured_charge} = Charge.capture(uncaptured_charge, client(address))
+
+        assert captured_charge.captured == true
+      end
+    end
+
+    test "Capture a partial charge", %{charge: charge, card: card} do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/capture_partial_charge.fixture") do
+        {:ok, uncaptured_charge} =
+          Charge.create(%{charge | capture: false, card: card}, client(address))
+
+        assert uncaptured_charge.captured == false
+
+        {:error, err} =
+          Charge.capture(uncaptured_charge, %{amount: 200}, client(address))
+
+        assert err.error_description == "You must capture the full amount that was authorised"
+      end
+    end
+
+    test "Get all charges" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/get_all_charges.fixture") do
+        {:ok, charges} = Charge.get_all(client(address))
+
+        %{items: [charge | _]} = charges
+
+        assert charge.amount == 50000
+        assert length(charges.items) == 25
+      end
+    end
+
+    test "Get a specific charge", %{charge: charge, card: card} do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/get_charge.fixture") do
+        {:ok, created_charge} =
+          Charge.create(%{charge | card: card}, client(address))
+
+        {:ok, retreived_charge} = Charge.get(created_charge.token, client(address))
+
+        assert created_charge == retreived_charge
+      end
+    end
+
+    test "Search for a charge" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/search_charges.fixture") do
+        {:ok, retreived_charges} =
+          Charge.search([query: "hagrid@hogwarts.wiz"], client(address))
+
+        assert retreived_charges.count == 25
+        assert retreived_charges.items != []
       end
     end
   end

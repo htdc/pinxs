d0957bd2beaf39d7c2f3ce5bc99b376dbed969a3
Martin Feckie <mfeckie@gmail.com>
2020-06-02 10:50:19 +0800

    Add tests for webhooks with new client

--- test/webhooks/webhooks_test.exs
+++ test/webhooks/webhooks_test.exs
@@ -2,6 +2,8 @@ defmodule PINXS.Webhooks.WebhookTest do
   use ExUnit.Case, async: true
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
   alias PINXS.Webhooks.Webhook
+  use Nug
+  import PINXS.TestHelpers
 
   test "Creates a webhook" do
     use_cassette("webhooks/create") do
@@ -54,6 +56,65 @@ defmodule PINXS.Webhooks.WebhookTest do
       {:ok, result} = Webhook.delete(webhook.token, PINXS.config("api_key", :test))
 
       assert result
+    end
+  end
+
+  describe "new client" do
+    test "Creates a webhook" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/webhooks/create.fixture") do
+        {:ok, webhook} =
+          Webhook.create(
+            %Webhook{url: "https://www.example.com/webhooks"},
+            client(address)
+          )
+
+        assert webhook.token != nil
+      end
+    end
+
+    test "Retrieve webooks" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/webhooks/get_all.fixture") do
+        client = client(address)
+        {:ok, webhook} =
+          Webhook.create(
+            %Webhook{url: "https://www.example.com/webhooks2"},
+            client
+          )
+
+        {:ok, [retreived_hook | _ ]} = Webhook.get(client)
+
+        assert webhook == retreived_hook
+      end
+    end
+
+    test "Retrieve specific webhook" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/webhooks/get.fixture") do
+        client = client(address)
+        {:ok, webhook} =
+          Webhook.create(
+            %Webhook{url: "https://www.example.com/webhooks3"},
+            client
+          )
+
+        {:ok, retreived_hook} = Webhook.get(webhook.token, client)
+
+        assert webhook == retreived_hook
+      end
+    end
+
+    test "Delete webhook" do
+      with_proxy(PINXS.Client.test_url(), "test/fixtures/webhooks/delete.fixture") do
+        client = client(address)
+        {:ok, webhook} =
+          Webhook.create(
+            %Webhook{url: "https://www.example.com/webhooks4"},
+            client
+          )
+
+        {:ok, result} = Webhook.delete(webhook.token, client)
+
+        assert result
+      end
     end
   end
 end

f6e1947149144c71610f0338f296bff1d10900e8
Martin Feckie <mfeckie@gmail.com>
2020-06-09 14:51:40 +0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PINXS.MixProject do
   def project do
     [
       app: :pinxs,
-      version: "2.1.0",
+      version: "3.0.0",
       elixir: "~> 1.6",
       source_url: "https://github.com/htdc/pinxs",
       description: """

a45f16f6f3e43c3a9378570b3507778205ce8337
Martin Feckie <mfeckie@gmail.com>
2020-06-09 15:30:20 +0800

    Remove old API references

--- lib/http/api.ex
+++ lib/http/api.ex
@@ -1,5 +1,4 @@
 defmodule PINXS.HTTP.API do
-  alias PINXS.HTTP.ClientBase, as: Client
   alias PINXS.HTTP.Response
 
   @moduledoc """
@@ -10,45 +9,25 @@ defmodule PINXS.HTTP.API do
   are available where extreme customisation is desired
   """
 
-  def delete(url, %PINXS{} = config), do: Client.authenticated_delete(url, config)
   def delete(url, %Tesla.Client{} = client), do: Tesla.delete(client, url)
-
-  def delete(url, module, %PINXS{} = config),
-    do: delete(url, config) |> Response.transform(module)
 
   def delete(url, module, %Tesla.Client{} = client),
     do: Tesla.delete(client, url) |> Response.transform(module)
 
-  def get(url, %PINXS{} = config), do: Client.authenticated_get(url, config)
   def get(url, %Tesla.Client{} = client), do: Tesla.get(client, url)
-
-  def get(url, module, %PINXS{} = config),
-    do: Client.authenticated_get(url, config) |> Response.transform(module)
 
   def get(url, module, %Tesla.Client{} = client),
     do: Tesla.get(client, url) |> Response.transform(module)
 
-  def search(url, params, module, %PINXS{} = config),
-    do: Client.authenticated_search(url, params, config) |> Response.transform(module)
-
   def search(url, params, module, %Tesla.Client{} = client),
     do: Tesla.get(client, url, query: params) |> Response.transform(module)
 
-  def post(url, params, %PINXS{} = config), do: Client.authenticated_post(url, params, config)
-
   def post(url, params, %Tesla.Client{} = client), do: Tesla.post(client, url, params)
-
-  def post(url, params, module, %PINXS{} = config),
-    do: post(url, params, config) |> Response.transform(module)
 
   def post(url, params, module, %Tesla.Client{} = client),
     do: post(url, params, client) |> Response.transform(module)
 
-  def put(url, params, %PINXS{} = config), do: Client.authenticated_put(url, params, config)
   def put(url, params, %Tesla.Client{} = client), do: Tesla.put(client, url, params)
-
-  def put(url, params, module, %PINXS{} = config),
-    do: put(url, params, config) |> Response.transform(module)
 
   def put(url, params, module, %Tesla.Client{} = client),
     do: put(url, params, client) |> Response.transform(module)

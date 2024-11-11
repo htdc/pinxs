532df8cc8294f4dacb52b290d977b97a19bf27ab
Brennan Fairfull <brennanf@hotdoc.com.au>
2024-11-11 18:43:01 +1000

    update pin host
    api.pin.net.au host will not resolve from 1st Dec 2024

--- lib/pinxs.ex
+++ lib/pinxs.ex
@@ -14,18 +14,18 @@ defmodule PINXS do
   Default usage is to just provide the api key
 
       iex> PINXS.config("ABC123")
-      %PINXS{api_key: "ABC123", url: "https://api.pin.net.au/1"}
+      %PINXS{api_key: "ABC123", url: "https://api.pinpayments.com/1"}
   """
 
   def config(api_key) do
-    %PINXS{api_key: api_key, url: "https://api.pin.net.au/1"}
+    %PINXS{api_key: api_key, url: "https://api.pinpayments.com/1"}
   end
 
   @doc """
   If you want to use test mode
 
       iex> PINXS.config("ABC123", :test)
-      %PINXS{api_key: "ABC123", url: "https://test-api.pin.net.au/1"}
+      %PINXS{api_key: "ABC123", url: "https://test-api.pinpayments.com/1"}
 
   Or you can have an arbitrary URL
 
@@ -33,7 +33,7 @@ defmodule PINXS do
       %PINXS{api_key: "ABC123", url: "https://my-fake-pin"}
   """
   def config(api_key, :test) do
-    %PINXS{api_key: api_key, url: "https://test-api.pin.net.au/1"}
+    %PINXS{api_key: api_key, url: "https://test-api.pinpayments.com/1"}
   end
 
   def config(api_key, url) do
